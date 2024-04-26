0a73cfb691a326e79ba8b5939984767865224dfa
Dhanushs3103 <dhanushvictor2003@gmail.com>
2024-04-26 13:32:55 +0530

    added the code

--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,12 +1,31 @@
 let form = document.querySelector("form");
-let userName = document.querySelector("#userName");
-let userEmail = document.querySelector("#userEmail");
+let userName = document.querySelector("#username");
+let userEmail = document.querySelector("#email");
 let userPassword = document.querySelector("#password");
 let passwordConfirmation = document.querySelector("#confirmPassword");
 
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-    event.preventDefault();
-    
+async function handleSubmit(e) {
+    e.preventDefault(); 
+    if(userName.value && userEmail.value && userPassword.value && passwordConfirmation.value) {
+        if(userPassword.value === passwordConfirmation.value) {
+           let userData = await fetch ("http://localhost:3000/users",{
+               method: "POST",
+               headers: {
+                   "Content-Type": "application/json"
+               },
+               body: JSON.stringify({
+                   name: userName.value,
+                   email: userEmail.value,
+                   password: userPassword.value
+               })
+           })
+           console.log(userData);
+        }else {
+            alert("Password and confirm password should be same");
+        }
+    }else {
+        alert("Please fill all the fields");
+    }  
 }
